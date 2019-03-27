375aa87a5173395a2be7944ded104a80cd9bee08
Rodrigo <dulo.rodrigo@gmail.com>
2019-03-27 17:57:02 -0600

    Crea servicio, error conexión en socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { environment } from '../environments/environment';
+
 // Sockets
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 const config: SocketIoConfig = {
@@ -10,7 +13,6 @@ const config: SocketIoConfig = {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
